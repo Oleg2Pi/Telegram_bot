e97c4ff3b06793a122da9c4e86b2e987a5edb123
Oleg2Pi <polikarpovoleg115@gmail.com>
2023-11-06 09:43:41 +0300

    Finished

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import telebot
-import requests
-import json
 from config import TOKEN, keys
-from utils import APIException, CryptoConverter
+from extensions import APIException, CryptoConverter
 
 
 bot = telebot.TeleBot(TOKEN)
-
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message: telebot.types.Message):
@@ -16,14 +13,12 @@ def start(message: telebot.types.Message):
 Чтобы увидеть список доступных валют, введите /values'
     bot.reply_to(message, text)
 
-
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
         text = '\n'.join((text, key))
     bot.reply_to(message, text)
-
 
 @bot.message_handler(content_types=['text'])
 def convert(message: telebot.types.Message):
@@ -45,6 +40,5 @@ def convert(message: telebot.types.Message):
     else:
         text = f'Цена на {amount} {base} в {quote} составляет {total_quote}'
         bot.send_message(message.chat.id, text)
-    
     
 bot.polling(non_stop=True)
